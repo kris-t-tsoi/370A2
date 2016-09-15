13ed73c80ef7d73f48b3e434624814c798ee24ab
kris-t-tsoi <kris.t.tsoi@gmail.com>
2016-09-15 13:59:18 +1200

    create tinydos and volume class

---
+++ TinyDOS.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+class TinyDOS:
+
+    if __name__ == '__main__':
+
+        #read argument and pass to funtion
+
+        pass
+
+
+
+    def format(self):
+        pass
+
+    def reconnect(self):
+        pass
+
+    def list(self):
+        pass
+
+    def makeFile(self):
+        pass
+
+
+    def makeDirectory(self):
+        pass
---
+++ volume.py
@@ -0,0 +1,6 @@
+import os
+
+class Volume:
+
+    def __init__(self):
+        pass
