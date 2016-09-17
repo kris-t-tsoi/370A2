8e6ecea1304253f329f5f3a083456c38469a93ea
kris-t-tsoi <kris.t.tsoi@gmail.com>
2016-09-17 12:00:30 +1200

    add rest of sekelton code into tinydos

--- TinyDOS.py
+++ TinyDOS.py
@@ -3,14 +3,6 @@ import sys
 
 class TinyDOS:
 
-    if __name__ == '__main__':
-
-        #read argument and pass to funtion
-
-        pass
-
-
-
     def format(self):
         pass
 
@@ -26,3 +18,32 @@ class TinyDOS:
 
     def makeDirectory(self):
         pass
+
+    def appendToFile(self):
+        pass
+
+    def printFile(self):
+        pass
+
+
+    def deleteFile(self):
+        pass
+
+    def deleteDirectory(self):
+        pass
+
+    def quitProgram(self):
+
+        #close file
+
+        #exit program
+        pass
+
+
+if __name__ == '__main__':
+    # read argument and pass to funtion
+    print(len(sys.argv))
+    print(sys.argv)
+
+    pass
+
