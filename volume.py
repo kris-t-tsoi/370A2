5f2a8865351e9dca1acf7571d3ea8b38f4098cc2
kris-t-tsoi <kris.t.tsoi@gmail.com>
2016-09-21 19:01:01 +1200

    created inital block 0 data

--- volume.py
+++ volume.py
@@ -1,4 +1,5 @@
 import os
+import drive
 
 #Yee Wing Kristy Tsoi
 #ytso868
@@ -6,18 +7,19 @@ import os
 
 class Volume:
 
-    BLK_SIZE = 512
-    DRIVE_SIZE = 128
-
+    #icons
     EMPTY_BLK_ICON = '-'
     USED_BLK_ICON = '+'
+    FILE_ICON = 'f:'
+    DIRECTORY_ICON = 'd:'
 
     MAX_FILE_NAME_SIZE = 8
     MAX_FILE_BLOCK_USE = 12
+    FILE_DETAIL_SIZE = 64
 
-    driveBitMapString = ""
-    dataToWrite = ""
-    dataRead = ""
+    driveBitMapString = ''
+    dataToWrite = ''
+    dataRead = ''
 
     def __init__(self, name):
         self.name = name
@@ -30,10 +32,11 @@ class Volume:
         data = Volume.USED_BLK_ICON + (Volume.EMPTY_BLK_ICON*127)
         self.dataToWrite = data
 
-        #write 6 empty file
+        #write 6 empty file details
+        for x in range(0,6):
+            self.dataToWrite = self.dataToWrite +self.FILE_ICON+(' '*self.MAX_FILE_NAME_SIZE)+' '+('0'*4)+':'+((('0'*3)+' ')*self.MAX_FILE_BLOCK_USE)
+            pass
 
-
-        self.finishFormatingBlockData()
         return self.dataToWrite
 
     def getBitmapString(self):
@@ -44,7 +47,7 @@ class Volume:
     #adds spaces to fill up the rest of the block for writing
     def finishFormatingBlockData(self):
         i = len(self.dataToWrite)
-        sizeToAdd = self.BLK_SIZE - i
+        sizeToAdd = drive.Drive.BLK_SIZE - i
         self.dataToWrite = self.dataToWrite + (' '*sizeToAdd)
 
 
